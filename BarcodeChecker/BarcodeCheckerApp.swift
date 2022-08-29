37d9f2b7b94238de779ba540df9764dbc133c63c
Rokas P <petrulionis.rokas@gmail.com>
2022-08-29 14:11:54 +0300

    Initial Commit

---
+++ BarcodeChecker/BarcodeCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeCheckerApp.swift
+//  BarcodeChecker
+//
+//  Created by Rokas Petrulionis on 2022-08-29.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
