48e89d1299655a08a16d893a2727695872e0140a
Rokas P <petrulionis.rokas@gmail.com>
2022-08-29 14:58:41 +0300

    Initial Commit

---
+++ BarcodeChecker/Model/Barcode.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode.swift
+//  BarcodeChecker
+//
+//  Created by Rokas Petrulionis on 2022-08-29.
+//
+
+import SwiftUI
+
+
+struct Product: Identifiable{
+    var id = UUID()
+    var productName: String
+    var barcode: String
+    var price: Float
+    var store: Float
+}
